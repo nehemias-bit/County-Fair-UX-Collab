5ef70814316d5538cc368e68b5294cab366ba860
Ian Rappaport <ianrappaport@Ians-MacBook-Pro-2.local>
2019-11-17 07:49:10 -0500

    finished header and footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
-import Header from './components/Header'
-import Footer from './components/Footer'
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 // define our Event component
 class App extends React.Component {
