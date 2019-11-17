cbeb1a5ac1585bdbeddb0782660211aad16c8d19
Ian Rappaport <ianrappaport@Ians-MacBook-Pro-2.local>
2019-11-17 07:06:39 -0500

    updated my branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// define our Event component
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    // make sure to return some UI
+    return (
+      <div>
+        <h2>Basketball Practice</h2>
+        <p>7:45 - 9 pm</p>
+      </div >
+    );
+  }
 }
 
 export default App;
