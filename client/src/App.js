3c76aee848e1f4d32f1483ca49cc3ac2593f468b
Nehemias Cruz <nehemiascruz@Nehemiass-MacBook-Pro.local>
2019-11-18 20:04:06 -0500

    countdown is up and running

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
-import CountDownClock from './components/CountDownClock';
 
 // define our Event component
 class App extends React.Component {
@@ -19,8 +18,7 @@ class App extends React.Component {
     return (
       <div>
         <Header />
-        <Main />
-        <CountDownClock deadline={this.state.deadline} />
+        <Main deadline={this.state.deadline}/>
         <Footer />
       </div >
 
